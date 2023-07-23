671ba1ea3d5fead55b0997d5d6c43231b8dedba5
Dongwook kim <kdw1437@naver.com>
2023-07-24 00:38:04 +0900

    Updata calendar main()

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,10 +1,9 @@
 package honux.calendar;
 
 public class Calendar {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, Calendar");
+		System.out.println("Hello");
+		System.out.println("hi");
+		System.out.println("kee");
 	}
-
 }
