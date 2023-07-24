01203a2fe76d6e4dbad2c4166ec90c626eaa3a73
Dongwook kim <kdw1437@naver.com>
2023-07-24 23:18:05 +0900

    Update using class method

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,9 +1,30 @@
 package honux.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("hi");
-		System.out.println("kee");
+	
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getmaxDaysOfMonth(int month) {
+		return MAX_DAYS[month -1];
 	}
-}
+	
+	public void printSampleCalendar() {
+		System.out.println("1, 2, 3, 4");
+	}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+		System.out.println("달을 입력하세요.");
+		int month = scanner.nextInt(); //scanner인스턴스에 non static 메소드인 nextInt를 적용해서 scnnaer의 int type의 필드값을 얻어냄.
+		
+		
+		
+		System.out.printf("%d 월은 %d 일까지 있습니다.\n", month, cal.getmaxDaysOfMonth(month));
+		
+		cal.printSampleCalendar();
+		scanner.close();
+		}
+	}
+
