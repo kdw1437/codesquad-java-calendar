8c1f47d3ed8c5e6160a149f5b020c8724f377114
Dongwook kim <kdw1437@naver.com>
2023-07-24 23:39:08 +0900

    calendar while break continue

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -14,17 +14,26 @@ public class Calendar {
 		System.out.println("1, 2, 3, 4");
 	}
 	public static void main(String[] args) {
+		String PROMPT = "cal>";
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		
-		System.out.println("반복 횟수를 입력하세요.");
-		int repeat = scanner.nextInt();
+		//System.out.println("반복 횟수를 입력하세요.");
 		
-		for (int i = 0; i<repeat; i++) {
+		
+		int month = 1;
+		
+		while (true) {
 			System.out.println("달을 입력하세요.");
-			int month = scanner.nextInt(); //scanner인스턴스에 non static 메소드인 nextInt를 적용해서 scnnaer의 int type의 필드값을 얻어냄.
-			
-			
+			System.out.print(PROMPT);
+			month = scanner.nextInt(); //scanner인스턴스에 non static 메소드인 nextInt를 적용해서 scnnaer의 int type의 필드값을 얻어냄.
+			if (month == -1) {
+				break;
+			}
+			if (month > 12)
+			{
+				continue;
+			}
 			
 			System.out.printf("%d 월은 %d 일까지 있습니다.\n", month, cal.getmaxDaysOfMonth(month));
 		
